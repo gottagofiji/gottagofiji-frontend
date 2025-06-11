0a43a9d1d1f4d82713688b01eff11e13ee3eb9e3
gottagofiji <shabeelshah@outlook.com>
2025-06-11 17:50:34 +1000

    fix:updated pages

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,1 @@
-import React from "react";
-import HomepageAds from "../components/HomepageAds";
-import FeaturedListings from "../components/FeaturedListings";
-import SearchForm from "../components/SearchForm";
-import SearchResults from "../components/SearchResults";
-
-const Home = () => {
-  return (
-    <div className="mt-4">
-      <HomepageAds />
-      <SearchForm />
-      <FeaturedListings />
-      <SearchResults />
-    </div>
-  );
-};
-
-export default Home;
+// Placeholder content for Home.jsx
---
+++ src/pages/HomepageAds.jsx
@@ -0,0 +1,1 @@
+// Placeholder content for HomepageAds.jsx
